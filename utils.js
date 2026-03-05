f127638f14f8dc1c039c2cc7710616edd482d820
bumbumchv-prog <bumbumchv@gmail.com>
2026-03-05 20:29:17 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:29:09.743Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T17:29:18.336Z
+Commit: Fix CSS styling bug on mobile devices
