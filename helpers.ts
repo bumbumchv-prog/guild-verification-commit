b5b97f7f554abf31ec88527feb377803b3a28ee0
bumbumchv-prog <bumbumchv@gmail.com>
2026-03-05 20:32:07 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:32:04.813Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T17:32:08.757Z
+Commit: Fix memory leak in event handlers
