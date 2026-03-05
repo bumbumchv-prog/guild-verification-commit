8b6637fa9c8aad19dac051888637f4358bcae7da
bumbumchv-prog <bumbumchv@gmail.com>
2026-03-05 20:34:21 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:33:23.432Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T17:34:22.558Z
+Commit: Fix memory leak in event handlers
