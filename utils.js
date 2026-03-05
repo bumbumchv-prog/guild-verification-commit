c710dd30c8ced0f86a686463694f5c6d45fed317
bumbumchv-prog <bumbumchv@gmail.com>
2026-03-05 20:29:32 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:29:18.336Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T17:29:33.215Z
+Commit: Add export functionality for reports
