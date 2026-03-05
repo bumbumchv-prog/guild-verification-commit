26a7bc5b2197c4ddc944d840ec9c7b712bab5932
bumbumchv-prog <bumbumchv@gmail.com>
2026-03-05 20:31:21 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:31:12.486Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T17:31:22.683Z
+Commit: Fix memory leak in event handlers
