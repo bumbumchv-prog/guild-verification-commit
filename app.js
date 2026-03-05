9c6b9e51dcf0e194d40ac1c721785e035f2da83f
bumbumchv-prog <bumbumchv@gmail.com>
2026-03-05 20:30:55 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:30:54.543Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T17:30:57.229Z
+Commit: Fix broken links in documentation
