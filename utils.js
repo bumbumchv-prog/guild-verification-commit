827d45f025e9fa09fb7f31af95458c2dcd5c7cff
bumbumchv-prog <bumbumchv@gmail.com>
2026-03-05 20:33:49 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:33:37.418Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T17:33:51.039Z
+Commit: Fix broken links in documentation
