7426e1fe2a6fe4b8c90a5961e8452b7fd009a4cf
bumbumchv-prog <bumbumchv@gmail.com>
2026-03-05 20:32:03 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:31:37.242Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T17:32:04.813Z
+Commit: Patch API endpoint error handling
