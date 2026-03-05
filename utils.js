3b6a50795afa810f38ef511c2cff994de67f9aeb
bumbumchv-prog <bumbumchv@gmail.com>
2026-03-05 20:31:59 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:31:23.772Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T17:32:00.837Z
+Commit: Fix broken links in documentation
