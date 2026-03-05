a7aa5e379341afa5f5425c8391c46e16fb1b5f34
bumbumchv-prog <bumbumchv@gmail.com>
2026-03-05 20:32:24 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:32:17.825Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T17:32:26.317Z
+Commit: Fix broken links in documentation
