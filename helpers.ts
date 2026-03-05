ae6adc98a39f8b86890f4c9d6a6136b22b97582d
bumbumchv-prog <bumbumchv@gmail.com>
2026-03-05 20:33:11 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:32:50.286Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T17:33:12.700Z
+Commit: Patch API endpoint error handling
